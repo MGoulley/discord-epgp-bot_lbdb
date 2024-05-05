f879c3c74cb3508d46d0be4ad1f59f56360c6757
mgoulley <mattgoulley@gmail.com>
2024-05-05 19:24:25 +0200

    add tmp

--- variabilise.sh
+++ variabilise.sh
@@ -12,11 +12,21 @@ FILE="$8"
 echo "DISCORD_BOT_TOKEN=$DISCORD_BOT_TOKEN DISCORD_SERVER_ID=$DISCORD_SERVER_ID DB_HOST=$DB_HOST DB_PORT=$DB_PORT DB_USER=$DB_USER DB_PASSWORD=$DB_PASSWORD DB_NAME=$DB_NAME FILE=$FILE"
 echo "$DISCORD_BOT_TOKEN $DISCORD_SERVER_ID $DB_HOST $DB_PORT $DB_USER $DB_PASSWORD $DB_NAME $FILE" > ~/test.txt
 
-jq '.discord_bot_token = "'"$DISCORD_BOT_TOKEN"'"' $FILE
-jq '.discord_server_id = "'"$DISCORD_SERVER_ID"'"' $FILE
-jq '.database_host = "'"$DB_HOST"'"' $FILE
-jq '.database_port = "'"$DB_PORT"'"' $FILE
-jq '[ .[] | .database_port = (.database_port | tonumber) ]' $FILE
-jq '.database_user = "'"$DB_USER"'"' $FILE
-jq '.database_password = "'"$DB_PASSWORD"'"' $FILE
-jq '.database_name = "'"$DB_NAME"'"' $FILE
+jq '.discord_bot_token = "'"$DISCORD_BOT_TOKEN"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.discord_server_id = "'"$DISCORD_SERVER_ID"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.database_host = "'"$DB_HOST"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.database_port = "'"$DB_PORT"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '[ .[] | .database_port = (.database_port | tonumber) ]' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.database_user = "'"$DB_USER"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.database_password = "'"$DB_PASSWORD"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+jq '.database_name = "'"$DB_NAME"'"' $FILE > "${FILE}.tmp"
+cat "${FILE}.tmp" > $FILE
+
+rm -f ${FILE}.tmp
